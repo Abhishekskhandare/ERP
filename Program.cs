0913335a48535ad3335547f7e536c37986710684
abhishek.khandare <ASUS@LAPTOP-N2A120E1>
2026-08-31 21:13:57 +0530

    swagger setup

--- Program.cs
+++ Program.cs
@@ -3,10 +3,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+	options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+	options.RoutePrefix = string.Empty;
+	options.DocumentTitle = "My Swagger";
+});
 
 app.UseHttpsRedirection();
 
