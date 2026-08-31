f0bf442759978303caa98127697c288f1cd6e268
abhishek.khandare <ASUS@LAPTOP-N2A120E1>
2026-08-31 21:00:59 +0530

    basic changes

---
+++ Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		public UserController()
+		{
+		}
+
+
+		[HttpGet]
+		[Route("abhishek")]
+		public void GetUser()
+		{
+
+			// Implementation for getting user
+		}
+	}
+}
