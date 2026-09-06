79cd4c0b451e3cac585bb1cad8204513f7f95798
RutujaMule-29 <rutujamule67@gmail.com>
2026-09-06 14:08:17 +0530

    added

--- DTO/changePaaword.cs
+++ DTO/changePaaword.cs
@@ -5,6 +5,9 @@
         public string Email { get; set; } = null!;
         public string OldPassword { get; set; } = null!;
         public string NewPassword { get; set; } = null!;
+
+
+
     }
 
 }
